30f2fca453fa527086e4ee52a7baca23e1849d3b
Giusy Somma <giusy.somma93@gmail.com>
2025-05-22 23:19:57 +0200

    aggiunta foto

--- javascript.js
+++ javascript.js
@@ -1,17 +1,26 @@
-const track = document.getElementById('carouselTrack');
-const slides = document.querySelectorAll('.slide');
-let currentSlide = 0;
+const galleries = {
+  jilly: ['img/jilly1.jpg', 'img/jilly2.jpg', 'img/jilly3.jpg'],
+  pippo: ['img/pippo1.jpg', 'img/pippo2.jpg', 'img/pippo3.jpg'],
+  saverio: ['img/saverio1.jpg', 'img/saverio2.jpg', 'img/saverio3.jpg']
+};
 
-function updateSlidePosition() {
-  track.style.transform = `translateX(-${currentSlide * 100}%)`;
+const currentIndices = {
+  jilly: 0,
+  pippo: 0,
+  saverio: 0
+};
+
+function showImage(animal) {
+  const imgElement = document.getElementById(`${animal}-image`);
+  imgElement.src = galleries[animal][currentIndices[animal]];
 }
 
-function nextSlide() {
-  currentSlide = (currentSlide + 1) % slides.length;
-  updateSlidePosition();
+function nextImage(animal) {
+  currentIndices[animal] = (currentIndices[animal] + 1) % galleries[animal].length;
+  showImage(animal);
 }
 
-function prevSlide() {
-  currentSlide = (currentSlide - 1 + slides.length) % slides.length;
-  updateSlidePosition();
+function prevImage(animal) {
+  currentIndices[animal] = (currentIndices[animal] - 1 + galleries[animal].length) % galleries[animal].length;
+  showImage(animal);
 }
