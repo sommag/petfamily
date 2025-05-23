55e8e97e2e7eaf23480d4a5070accf5a0bdfdee8
Giusy Somma <giusy.somma93@gmail.com>
2025-05-23 12:01:24 +0200

    ottimizzazione layout

--- README.md
+++
@@ -1,8 +0,0 @@
-Demo online
-
-[Visualizza il sito su GitHub Pages](https://sommag.github.io/petfamily/)
-
-![HTML5](https://img.shields.io/badge/HTML5-E34F26?logo=html5&logoColor=white)
-![CSS3](https://img.shields.io/badge/CSS3-1572B6?logo=css3&logoColor=white)
-![JavaScript](https://img.shields.io/badge/JavaScript-F7DF1E?logo=javascript&logoColor=black)
-![GitHub Pages](https://img.shields.io/badge/Hosted%20on-GitHub%20Pages-121013?logo=githubpages)
---
+++ script.js
@@ -0,0 +1,37 @@
+alert("JS caricato!");
+
+const galleries = {
+  jilly: ['img/jilly11.jpg', 'img/jilly2.jpg', 'img/jilly3.jpg'],
+  pippo: ['img/pippo12.jpg', 'img/pippo2.jpg', 'img/pippo3.jpg'],
+  saverio: ['img/saverio.jpg', 'img/saverio2.jpg', 'img/saverio3.jpg']
+};
+
+let currentAnimal = '';
+let currentIndex = 0;
+
+function openModal(animal) {
+  currentAnimal = animal;
+  currentIndex = 0;
+  showPopupImage();
+  document.getElementById('popup-modal').style.display = 'flex';
+}
+
+function closeModal() {
+  document.getElementById('popup-modal').style.display = 'none';
+}
+
+function showPopupImage() {
+  const modalImg = document.getElementById('modal-image');
+  modalImg.src = galleries[currentAnimal][currentIndex];
+  modalImg.alt = `${currentAnimal} photo ${currentIndex + 1}`;
+}
+
+function nextPopupImage() {
+  currentIndex = (currentIndex + 1) % galleries[currentAnimal].length;
+  showPopupImage();
+}
+
+function prevPopupImage() {
+  currentIndex = (currentIndex - 1 + galleries[currentAnimal].length) % galleries[currentAnimal].length;
+  showPopupImage();
+}
